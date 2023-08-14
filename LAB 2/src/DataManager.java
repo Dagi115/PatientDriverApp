4826200e2fd2c5a6e0526579ffeaa5d72ecd3a5f
dagi <dagi@localhost>
2023-08-14 02:33:09 -0400

    First Commit

---
+++ LAB 2/src/DataManager.java
@@ -0,0 +1,18 @@
+public class DataManager {
+    
+	DataManager() {	}
+	
+	public String getHello() {
+		return "Hello World";
+	}
+
+	public String getHowdy() {
+		return "Howdy guys";
+	}
+
+	public String getChinese() {
+		return "Zuìjìn hǎo ma";
+	}
+
+
+}
